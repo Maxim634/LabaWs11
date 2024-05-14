9b9f7251f87aab57b47de1cac0c2ff16886be988
Максим <123723682+Maxim634@users.noreply.github.com>
2024-05-14 21:41:02 +0300

    Add files via upload

---
+++ Program 11.cs
@@ -0,0 +1,23 @@
+﻿using System;
+class Program
+{
+    delegate string GetNextDayOfWeek();
+
+    static void Main()
+    {
+        string[] daysOfWeek = { "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье" };
+        int currentDayIndex = -1;
+
+        GetNextDayOfWeek getNextDay = () =>
+        {
+            currentDayIndex = (currentDayIndex + 1) % daysOfWeek.Length;
+            return daysOfWeek[currentDayIndex];
+        };
+
+        for (int i = 0; i < 100; i++)
+        {
+            string nextDay = getNextDay();
+            Console.WriteLine("Следующий день недели: " + nextDay);
+        }
+    }
+}
